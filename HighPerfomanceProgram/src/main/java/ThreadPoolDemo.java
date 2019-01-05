d1a28e64bb338f45c2c9365ad7568d9cc095e7d9
Cdu <csddatamining@163.com>
2019-01-05 23:12:21 +0800

    [ADD]1.add ThreadPool Demo

---
+++ HighPerfomanceProgram/src/main/java/ThreadPoolDemo.java
@@ -0,0 +1,59 @@
+package main.java;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Cdu
+ * @discription: 线程池Demo
+ * @create 2019-01-05 23:05
+ */
+public class ThreadPoolDemo {
+    /**
+     * 测试： 提交15个执行时间需要3秒的任务,看线程池的状况
+     *
+     * @param threadPoolExecutor 传入不同的线程池，看不同的结果
+     * @throws Exception
+     */
+    public void testCommon(ThreadPoolExecutor threadPoolExecutor) throws Exception {
+        // 测试： 提交15个执行时间需要3秒的任务，看超过大小的2个，对应的处理情况
+        for (int i = 0; i < 15; i++) {
+            int n = i;
+            threadPoolExecutor.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        System.out.println("开始执行：" + n);
+                        Thread.sleep(3000L);
+                        System.err.println("执行结束:" + n);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            System.out.println("任务提交成功 :" + i);
+        }
+        // 查看线程数量，查看队列等待数量
+        Thread.sleep(500L);
+        System.out.println("当前线程池线程数量为：" + threadPoolExecutor.getPoolSize());
+        System.out.println("当前线程池等待的数量为：" + threadPoolExecutor.getQueue().size());
+        // 等待15秒，查看线程数量和队列数量（理论上，会被超出核心线程数量的线程自动销毁）
+        Thread.sleep(15000L);
+        System.out.println("当前线程池线程数量为：" + threadPoolExecutor.getPoolSize());
+        System.out.println("当前线程池等待的数量为：" + threadPoolExecutor.getQueue().size());
+    }
+
+    /**
+     * 1、线程池信息： 核心线程数量5，最大数量10，无界队列，超出核心线程数量的线程存活时间：5秒， 指定拒绝策略的
+     *
+     * @throws Exception
+     */
+    private void threadPoolExecutorTest1() throws Exception {
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(5, 10, 5, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<Runnable>());
+        testCommon(threadPoolExecutor);
+        // 预计结果：线程池线程数量为：5,超出数量的任务，其他的进入队列中等待被执行
+    }
+
+}
