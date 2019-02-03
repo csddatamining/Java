36cc3a955cac28ac5d48daa666968d7f079600e1
Cdu <csddatamining@163.com>
2019-02-03 23:52:12 +0800

    [ADD]add BIO note and demo

---
+++ chapter-2/chapter-2/src/main/java/com/study/hc/net/bio/BIOServer.java
@@ -0,0 +1,43 @@
+package com.study.hc.net.bio;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class BIOServer {
+
+    public static void main(String[] args) throws Exception {
+        //封装底层监听端口
+        ServerSocket serverSocket = new ServerSocket(8080);
+        System.out.println("服务器启动成功");
+        while (!serverSocket.isClosed()) {
+            Socket request = serverSocket.accept();// 阻塞（没有连接过来，会一直等待）
+            System.out.println("收到新连接 : " + request.toString());
+            try {
+                //  net + i/o 接收数据、打印
+                InputStream inputStream = request.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "utf-8"));
+                String msg;
+                while ((msg = reader.readLine()) != null) { // 没有数据，阻塞
+                    if (msg.length() == 0) {
+                        break;
+                    }
+                    System.out.println(msg);
+                }
+                System.out.println("收到数据,来自：" + request.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    request.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        serverSocket.close();
+    }
+}
