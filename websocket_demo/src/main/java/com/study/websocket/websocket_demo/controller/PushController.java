eeecc3cb7621fde1b4a7f0ca06add5897c099446
Cdu <csddatamining@163.com>
2021-04-07 00:08:36 +0800

    [ADD]add WebSocket push demo

---
+++ websocket_demo/src/main/java/com/study/websocket/websocket_demo/controller/PushController.java
@@ -0,0 +1,47 @@
+package com.study.websocket.websocket_demo.controller;
+
+import org.springframework.stereotype.Component;
+
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+/**
+ * @author Cdu
+ * @discription: WebSocet  for Server
+ * @create 2021-04-06 23:50
+ */
+@Component
+@ServerEndpoint(value = "/serverForWebSocket/{id}")
+public class PushController {
+
+    private Session session;
+
+    private static CopyOnWriteArraySet<Session> webSocketSet = new CopyOnWriteArraySet<>();
+
+    @OnOpen
+    public void onOpen(Session session) {
+        this.session = session;
+        webSocketSet.add(session);
+        System.out.println("Current connect user number:" + webSocketSet.size());
+    }
+
+    @OnMessage
+    public void onMessage(String msg, Session session) {
+        System.out.println(msg);
+        try {
+            sendMessage(msg, session);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMessage(String msg, Session session) throws IOException {
+        for (Session session1 : webSocketSet) {
+            session1.getBasicRemote().sendText(msg);
+        }
+    }
+}
